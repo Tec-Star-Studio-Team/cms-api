9cfec1763854bafeb4145e72f27209414562fc12
Tec Star Studio <TecStarStudio@users.noreply.github.com>
2026-09-16 19:13:31 -0300

    add template migration

---
+++ CmsApi/CmsApi.Server/Infrastructure/Persistence/Migrations/20260916220810_templateEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CmsApi.Server.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class templateEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                schema: "cms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Templates",
+                schema: "cms");
+        }
+    }
+}
